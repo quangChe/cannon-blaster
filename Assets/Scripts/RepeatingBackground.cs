010e599ce695c508a09982dd5ac0ffd5c69ea22e
quangChe <quang.n.che@gmail.com>
2019-10-17 16:05:58 -0700

    Added some enemy assets

--- Assets/Scripts/RepeatingBackground.cs
+++ Assets/Scripts/RepeatingBackground.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class RepeatingBackground : MonoBehaviour
 {
     private BoxCollider2D waterCollider;
-    private float bgHorizontalLength;
+    private float backgroundHorizontalLength;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        waterCollider = GetComponent<BoxCollider2D>();
+        backgroundHorizontalLength = waterCollider.size.x - 0.83138f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -backgroundHorizontalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(backgroundHorizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
     }
 }
