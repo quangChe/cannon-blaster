02f578e921075473f8a487124b59818b5af9ff1d
quangChe <quang.n.che@gmail.com>
2019-10-17 15:24:59 -0700

    Init

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D waterCollider;
+    private float bgHorizontalLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
