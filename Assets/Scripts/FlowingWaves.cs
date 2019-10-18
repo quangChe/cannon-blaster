3b988898dcb1dafe9175a9971b609ea9651cd900
quangChe <quang.n.che@gmail.com>
2019-10-18 12:03:15 -0700

    Completed setting up tilemap background for the fortress island. Also added wave effects.

---
+++ Assets/Scripts/FlowingWaves.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowingWaves : MonoBehaviour
+{
+    [SerializeField] float waveVelocity;
+    [SerializeField] float startPosition = 0.4f;
+    [SerializeField] float endPosition = -0.7f;
+    private Rigidbody2D waves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waves = GetComponent<Rigidbody2D>();
+        waves.velocity = new Vector2(0, waveVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= startPosition || transform.position.y <= endPosition)
+        {
+            waveVelocity = waveVelocity * -1;
+        }
+        waves.velocity = new Vector2(0, waveVelocity);
+    }
+}
