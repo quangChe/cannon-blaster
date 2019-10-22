ba73e50bb7c6c3b1c0d6ed1820fe6caea0c56a36
quangChe <quang.n.che@gmail.com>
2019-10-22 13:27:18 -0700

    Several game objects reskinned and set up a game scene.

---
+++ Assets/Scripts/WindTop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindTop : MonoBehaviour
+{
+    AreaEffector2D windConfig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        windConfig = GetComponent<AreaEffector2D>();
+    }
+
+    public void SetWind(float a, float sm)
+    {
+        this.windConfig.forceAngle = a;
+        this.windConfig.forceMagnitude = m;
+    }
+}
