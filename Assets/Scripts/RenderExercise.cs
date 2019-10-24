e00f9822ecd2c3c840a309b34168b1a66ce20ae2
quangChe <quang.n.che@gmail.com>
2019-10-24 13:37:38 -0700

    Added a render exercise script to dynamically render the exercise using sprite from ball configuration script.

---
+++ Assets/Scripts/RenderExercise.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderExercise : MonoBehaviour
+{
+    [SerializeField] GameObject exerciseObject;
+
+    public void SetExerciseSprite(Sprite ex)
+    {
+        exerciseObject.GetComponent<SpriteRenderer>().sprite = ex;
+    }
+}
