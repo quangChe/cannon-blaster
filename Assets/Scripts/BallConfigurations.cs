4c9c8e6bb196cc8ee5d1dd3c80b32080e6950c5e
quangChe <quang.n.che@gmail.com>
2019-10-24 13:34:36 -0700

    Set up a ball configuration to dynamically render the appropriate ball prefabs. Set up ball explosion to trigger explosion animation on a parachuting ball.

---
+++ Assets/Scripts/BallConfigurations.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallConfigurations : MonoBehaviour
+{
+    [SerializeField] GameObject ExerciseObject;
+
+    Sprite targetExercise;
+    float parachuteLiftForce;
+    float points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetParachuteLift(float n)
+    {
+        parachuteLiftForce = n;
+    }
+
+    public float GetParachuteLift()
+    {
+        return parachuteLiftForce;
+    }
+
+    public void SetExercise(Sprite exercise)
+    {
+        targetExercise = exercise;
+        ExerciseObject.GetComponent<SpriteRenderer>().sprite = exercise;
+    }
+
+    public Sprite GetExerciseSprite()
+    {
+        return targetExercise;
+    }
+}
