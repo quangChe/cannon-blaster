8d7c19c5b74a61a8586779c96aa1dee27ef2c7f0
quangChe <quang.n.che@gmail.com>
2019-10-24 13:32:30 -0700

    Refactored FireCannon to be a better entry-point for loading up level data.

--- Assets/Scripts/FireCannon.cs
+++ Assets/Scripts/FireCannon.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class FireCannon : MonoBehaviour
 {
-    [SerializeField] GameObject fastBall;
-    [SerializeField] GameObject medBall;
-    [SerializeField] GameObject slowBall;
+    [SerializeField] GameObject shotBall;
+    [SerializeField] Sprite targetExercise;
 
 
     // Start is called before the first frame update
@@ -17,30 +16,38 @@ public class FireCannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Instantiate(
-                slowBall,
-                new Vector2(transform.position.x + 0.387f, transform.position.y + 1.748f),
-                Quaternion.identity
-            );
+            BallConfigurations ballConfigs = InstantiateCannonBall();
+            ballConfigs.SetParachuteLift(20);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Instantiate(
-                medBall,
-                new Vector2(transform.position.x + 0.387f, transform.position.y + 1.748f),
-                Quaternion.identity
-            );
+            BallConfigurations ballConfigs = InstantiateCannonBall();
+            ballConfigs.SetParachuteLift(60);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            Instantiate(
-                fastBall,
+            BallConfigurations ballConfigs = InstantiateCannonBall();
+            ballConfigs.SetParachuteLift(140);
+        }
+
+    }
+
+    private BallConfigurations InstantiateCannonBall()
+    {
+        GameObject cannonBall = Instantiate(
+                shotBall,
                 new Vector2(transform.position.x + 0.387f, transform.position.y + 1.748f),
                 Quaternion.identity
             );
-        }
+        BallConfigurations ballConfigs = cannonBall.GetComponent<BallConfigurations>();
 
+        // Need to set this
+        ballConfigs.SetExercise(targetExercise);
+
+        return ballConfigs;
     }
 }
