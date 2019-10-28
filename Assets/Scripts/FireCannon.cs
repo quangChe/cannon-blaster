be658fa1e0f12d50d44d1a590d2f8346f6a08311
quangChe <quang.n.che@gmail.com>
2019-10-28 12:58:54 -0700

    Modified FireCannon script to be a dumb script that only fires game objects.

--- Assets/Scripts/FireCannon.cs
+++ Assets/Scripts/FireCannon.cs
@@ -5,35 +5,18 @@ using UnityEngine;
 public class FireCannon : MonoBehaviour
 {
     [SerializeField] GameObject shotBall;
-    [SerializeField] Sprite exerciseSprite;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            BallConfigurations ballConfigs = InstantiateCannonBall();
-            ballConfigs.SetParachuteLift(20);
-        }
-    }
-
-    private BallConfigurations InstantiateCannonBall()
+    public void NewBall(float fallDelay, Sprite exercise)
     {
         GameObject cannonBall = Instantiate(
                 shotBall,
                 new Vector2(transform.position.x + 0.387f, transform.position.y + 1.748f),
-                Quaternion.identity
+                transform.rotation
             );
+        Debug.Log(cannonBall.transform.position);
         BallConfigurations ballConfigs = cannonBall.GetComponent<BallConfigurations>();
-
-        // Need to set this
-        ballConfigs.SetExercise(exerciseSprite);
-
-        return ballConfigs;
+        ballConfigs.SetExercise(exercise);
+        ballConfigs.SetParachuteLift(fallDelay);
     }
 }
