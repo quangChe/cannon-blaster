f303e1e79cb9ffc08d63bfe5d516c8aa3e0e491e
quangChe <quang.n.che@gmail.com>
2019-10-29 10:33:14 -0700

    Created a mapping function that will mount the correct input script and run explosion trigger on a target ball object.

---
+++ Assets/Scripts/InputMapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMapper
+{
+    public static void MountScript(GameObject ball, GameObject explosion, string exercise)
+    {
+        if (exercise == "LS")
+        {
+            LightswitchInput script = ball.AddComponent<LightswitchInput>();
+            script.SetGameObjects(ball, explosion);
+        }
+        else if (exercise == "DK")
+        {
+            DoorknobInput script = ball.AddComponent<DoorknobInput>();
+            script.SetGameObjects(ball, explosion);
+        }
+        else if (exercise == "ZP")
+        {
+            ZipperInput script = ball.AddComponent<ZipperInput>();
+            script.SetGameObjects(ball, explosion);
+        }
+        else if (exercise == "CP")
+        {
+            CupInput script = ball.AddComponent<CupInput>();
+            script.SetGameObjects(ball, explosion);
+        }
+    }
+}
