7edf4b05a3fb4356797c841425dd3c12343bee4d
quangChe <quang.n.che@gmail.com>
2019-11-05 11:20:18 -0800

    Added a loading controller to the Home scene to toggle play button when game is ready

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingController : MonoBehaviour
+{
+    public Bluetooth bt;
+    public GameObject playButton;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (bt._connected)
+        {
+            GameObject button = Instantiate(playButton, transform.position, Quaternion.identity);
+            RectTransform objDimensions = button.GetComponent<RectTransform>();
+            objDimensions.SetParent(gameObject.transform.parent);
+            objDimensions.anchoredPosition = new Vector2(0, transform.position.y - 100f);
+            objDimensions.localScale = new Vector3(300, 180, 0);
+            Destroy(gameObject);
+        }
+    }
+}
