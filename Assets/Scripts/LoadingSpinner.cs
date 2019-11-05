6c71e98cc0c0d46e996b652d5a55b2bbcdb93454
quangChe <quang.n.che@gmail.com>
2019-11-05 11:13:52 -0800

    Added a new spinner sprite and created a spinner prefab with a loading spinner script to animate the spinner.

---
+++ Assets/Scripts/LoadingSpinner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingSpinner : MonoBehaviour {
+    public RectTransform _mainIcon;
+    public float _timeStep = 0.05f;
+    public float _oneStepAngle = 36;
+
+    float _startTime;
+	// Use this for initialization
+	void Start () {
+        _startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time - _startTime >= _timeStep)
+        {
+            Vector3 iconAngle = _mainIcon.localEulerAngles;
+            iconAngle.z += _oneStepAngle;
+
+            _mainIcon.localEulerAngles = iconAngle;
+
+            _startTime = Time.time;
+        }
+	}
+}
