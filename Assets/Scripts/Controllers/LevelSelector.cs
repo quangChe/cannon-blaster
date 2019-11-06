6a7989c6ba287f38893b19c08703dd40970029cf
quangChe <quang.n.che@gmail.com>
2019-11-06 12:33:54 -0800

    Built the LevelSelector script to populate the Level scene's Level Select Panel with levels that will be rendered dynamically from the count of level data JSON files and initiate the game with the correct file in LevelDataController.

---
+++ Assets/Scripts/Controllers/LevelSelector.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject levelHolder;
+    public GameObject levelIcon;
+
+    private int numberOfLevels = 50;
+    private Rect panelDimensions;
+    private Rect iconDimensions;
+    private int iconsPerPage;
+    private int gridPadding = 100;
+    private int iconSpacing = 50;
+
+    void Start()
+    {
+        panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
+        iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
+        int maxInARow = Mathf.FloorToInt(
+            (panelDimensions.width - 200) / (iconDimensions.width + iconSpacing)
+        );
+        int maxInACol = Mathf.FloorToInt(
+            panelDimensions.height / (iconDimensions.height + iconSpacing)
+        );
+        iconsPerPage = maxInARow * maxInACol;
+        int totalPages = Mathf.CeilToInt((float)numberOfLevels / (float)iconsPerPage);
+        LoadPanels(totalPages);
+    }
+
+    void LoadPanels(int numberOfPanels)
+    {
+        GameObject panelClone = Instantiate(levelHolder) as GameObject;
+
+        for (int i = 1; i <= numberOfPanels; i++)
+        {
+            GameObject panel = Instantiate(panelClone) as GameObject;
+            panel.transform.SetParent(gameObject.transform, false);
+            panel.transform.SetParent(levelHolder.transform);
+            panel.name = "Page " + i;
+            panel.GetComponent<RectTransform>().localPosition =
+                new Vector2(panelDimensions.width * (i - 1), 0);
+            bool lastPanel = i == numberOfPanels;
+            SetUpGrid(panel, lastPanel);
+            LoadIcons(iconsPerPage, panel, i);
+        }
+
+        Destroy(panelClone);
+    }
+
+    void SetUpGrid(GameObject panel, bool isLastPanel)
+    { 
+        GridLayoutGroup grid = panel.AddComponent<GridLayoutGroup>();
+        grid.cellSize = new Vector2(iconDimensions.width, iconDimensions.height);
+        grid.spacing = new Vector2(50, 50);
+        grid.padding.right = grid.padding.left = 100;
+        grid.childAlignment = (isLastPanel)
+            ? TextAnchor.MiddleLeft
+            : TextAnchor.MiddleCenter;
+    }
+
+    void LoadIcons(int numberOfIcons, GameObject parentObject, int page)
+    {
+        int startIndex = (page - 1) * iconsPerPage;
+        for (int i = startIndex + 1; i <= (startIndex + numberOfIcons); i++)
+        {
+            if (i > numberOfLevels) { break; }
+            GameObject icon = Instantiate(levelIcon) as GameObject;
+            icon.name = i.ToString();
+            icon.transform.SetParent(gameObject.transform, false);
+            icon.transform.SetParent(parentObject.transform);
+            icon.GetComponentInChildren<TextMeshProUGUI>().SetText(i.ToString());   
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
