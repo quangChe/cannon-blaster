063c9c6255d925d2b0c29b8fe035379756bbd7d8
quangChe <quang.n.che@gmail.com>
2019-11-08 09:45:04 -0800

    Initialized GameManager script with functionality to load all levels at start and maintain the state of the loadedLevel for the rest of the game.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; set; }
+
+    public Object[] levels;
+    public TextAsset loadedLevel;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        levels = Resources.LoadAll("levels", typeof(TextAsset));
+    }
+
+    public void LoadLevel(int levelNumber)
+    {
+        loadedLevel = (TextAsset)levels[levelNumber - 1];
+    }
+}
