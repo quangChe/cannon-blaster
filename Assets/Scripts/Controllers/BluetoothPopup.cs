794a16423d0c509c9fa1768142cb4e7192be3a8a
quangChe <quang.n.che@gmail.com>
2019-11-11 11:45:15 -0800

    Created a BluetoothPopup controller that will provide game with a modular popup to pause the current game state and notify that bluetooth connection is being (re)established.

---
+++ Assets/Scripts/Controllers/BluetoothPopup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluetoothPopup : MonoBehaviour
+{
+    private BluetoothManager bt;
+    private bool popUpActive = false;
+    private GameObject activePopUp;
+
+    public GameObject bluetoothPopup;
+
+    private void Start()
+    {
+        bt = BluetoothManager.Instance;
+    }
+    void Update()
+    {
+        if (!bt.connected && !popUpActive)
+        {
+            PopUpConnectionNotice();
+        }
+
+        if (bt.connected && popUpActive)
+        {
+            ClosePopUp();
+        }
+    }
+
+    private void PopUpConnectionNotice()
+    { 
+        popUpActive = true;
+        Time.timeScale = 0;
+        activePopUp = Instantiate(bluetoothPopup, new Vector3(0, 0, -9), transform.rotation);
+    }
+
+    private void ClosePopUp()
+    {
+        popUpActive = false;
+        Destroy(activePopUp);
+        Time.timeScale = 1;
+    }
+}
