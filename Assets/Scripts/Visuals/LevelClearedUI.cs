592d735f1876277c94f5f750e133b9d48eeabe9e
quangChe <quang.n.che@gmail.com>
2019-11-13 16:05:25 -0800

    More cool animations for reward players with potential for providing different types of praises based off their performance.

--- Assets/Scripts/Visuals/LevelClearedUI.cs
+++ Assets/Scripts/Visuals/LevelClearedUI.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelClearedUI : MonoBehaviour
 {
     public LevelDataController levelData;
     public GameObject star1, star2, star3;
     public GameObject praiseText;
+    public GameObject hiddenDetails;
     public AudioClip starSound, praiseSound;
 
     void Start()
     {
+        hiddenDetails.SetActive(false);
         StartCoroutine(AnimateAllStars());
     }
 
@@ -20,10 +24,13 @@ public class LevelClearedUI : MonoBehaviour
         StartCoroutine(AnimateStars(star1));
         yield return new WaitForSeconds(.5f);
         StartCoroutine(AnimateStars(star2));
-        yield return new WaitForSeconds(.5f);
-        //StartCoroutine(AnimatePraise());
+        yield return new WaitForSeconds(.7f);
+        //PraisePlayer();
+        StartCoroutine(AnimatePraiseText("Yay. Wow."));
+        yield return new WaitForSeconds(.7f);
     }
 
+ 
     private IEnumerator AnimateStars(GameObject star)
     {
         RectTransform starTransform = star.GetComponent<RectTransform>();
@@ -44,5 +51,36 @@ public class LevelClearedUI : MonoBehaviour
 
     }
 
+    private void PraisePlayer()
+    {
+        float percent = levelData.successPercent;
+        if (percent >= 30f && percent < 60f)
+        {
+            //praiseText.GetComponent<TextMeshProUGUI>.SetText("")
+        }
+        else if (percent >= 60f && percent < 95f)
+        {
+
+        } else if (percent >= 95f)
+        {
+
+        }
+    }
+
+    private IEnumerator AnimatePraiseText(string praise)
+    {
+        TextMeshProUGUI text = praiseText.GetComponent<TextMeshProUGUI>();
+        text.fontSize = 1;
+        text.SetText(praise);
+        while(text.fontSize < 100f)
+        {
+            text.fontSize += 5f;
+            yield return null;
+        }
+
+        AudioSource.PlayClipAtPoint(praiseSound, Camera.main.transform.position, 1f);
+    }
+
+   
 
 }
