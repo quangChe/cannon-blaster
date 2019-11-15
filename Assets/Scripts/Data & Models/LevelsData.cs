f748f33d511f36232206993d6eb8664c724a3608
quangChe <quang.n.che@gmail.com>
2019-11-15 15:11:11 -0800

    Made changes to the LevelsModel with optional date field. Added connection, table creation, indexing of level number, add, and read functionality to the LevelsData for SQLite database.

--- Assets/Scripts/Data & Models/LevelsData.cs
+++ Assets/Scripts/Data & Models/LevelsData.cs
@@ -1,17 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Database
 {
-    public class LevelsData
+    public class LevelsData : SqliteHelper
     {
-        public int _id; // Also the level number
-        public int _starsCollected;
-        public int _successCount;
-        public int _totalCount;
-        public string _date;
+        private const string TABLE_NAME = "Levels";
+        private const string KEY_LEVEL = "level_number";
+        private const string KEY_STARS = "stars_collected";
+        private const string KEY_SUCCESS = "success_count";
+        private const string KEY_TOTAL = "total_count";
+        private const string KEY_DATE = "date";
+
+        private string[] COLUMNS = {KEY_LEVEL, KEY_STARS, KEY_SUCCESS, KEY_TOTAL};
+
+        public LevelsData() : base()
+        {
+            IDbCommand cmd = getDbCommand();
+            cmd.CommandText = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                "( id INTEGER PRIMARY KEY," +
+                KEY_LEVEL + " INTEGER, " +
+                KEY_STARS + " INTEGER, " +
+                KEY_SUCCESS + " INTEGER, " +
+                KEY_TOTAL + " INTEGER, " +
+                KEY_DATE + " DATETIME DEFAULT CURRENT_TIMESTAMP );";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "CREATE UNIQUE INDEX IF NOT EXISTS idx_level_number ON " +
+                TABLE_NAME + " (" + KEY_LEVEL + ");";
+            cmd.ExecuteNonQuery();
+        }
+
+        public void addData(LevelsModel level)
+        {
+            IDbCommand cmd = getDbCommand();
+            cmd.CommandText = "REPLACE INTO " + TABLE_NAME + " ( " +
+                KEY_LEVEL + ", " +
+                KEY_STARS + ", " +
+                KEY_SUCCESS + ", " +
+                KEY_TOTAL + " ) " +
+                "VALUES ( " +
+                level._levelNumber + ", " +
+                level._starsCollected + ", " +
+                level._successCount + ", " +
+                level._totalCount + " );";
+            cmd.ExecuteNonQuery();
+        }
+
+        public override IDataReader getAllData()
+        {
+            return base.getAllData(TABLE_NAME);
+        }
+
+
     }
 }
 
---
+++ Assets/Scripts/Data & Models/LevelsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Database
+{
+    public class LevelsModel
+    {
+        public int _levelNumber; // Also the level number
+        public int _starsCollected;
+        public int _successCount;
+        public int _totalCount;
+        public string _date = "";
+
+        public LevelsModel(int levelNumber, int starsCollected, int successCount, int totalCount)
+        {
+            _levelNumber = levelNumber;
+            _starsCollected = starsCollected;
+            _successCount = successCount;
+            _totalCount = totalCount;
+        }
+
+        public LevelsModel(int lvl, int stars, int success, int total, string date)
+            : this(lvl, stars, success, total)
+        {
+            _date = date;
+        }
+    }
+}
+
