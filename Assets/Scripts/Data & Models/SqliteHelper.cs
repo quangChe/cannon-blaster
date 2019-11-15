ecb10b8a698726bc571da307a441fafce599bc2d
quangChe <quang.n.che@gmail.com>
2019-11-14 16:53:59 -0800

    Added SqliteHelper script. More testing needs to be done on this with a custom Player data model and a PlayerDb script to create Player table with data to fill.

---
+++ Assets/Scripts/Data & Models/SqliteHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Data.Sqlite;
+using UnityEngine;
+using System.Data;
+
+namespace Database
+{
+    public class SqliteHelper
+    {
+        private const string Tag = "Riz: SqliteHelper:\t";
+
+        private const string dbName = "fitmi_test";
+
+        public string dbPath;
+        public IDbConnection dbConnection;
+
+        public SqliteHelper()
+        {
+            dbPath = "URI=file:" + Application.persistentDataPath + "/" + dbName;
+            Debug.Log("dbPath" + dbPath);
+            dbConnection = new SqliteConnection(dbPath);
+            dbConnection.Open();
+        }
+
+        ~SqliteHelper()
+        {
+            dbConnection.Close();
+        }
+
+        // virtual functions
+        public virtual IDataReader getDataById(int id)
+        {
+            Debug.Log(Tag + "This function is not implemnted");
+            throw null;
+        }
+
+        public virtual IDataReader getDataByString(string str)
+        {
+            Debug.Log(Tag + "This function is not implemnted");
+            throw null;
+        }
+
+        public virtual void deleteDataById(int id)
+        {
+            Debug.Log(Tag + "This function is not implemented");
+            throw null;
+        }
+
+        public virtual void deleteDataByString(string id)
+        {
+            Debug.Log(Tag + "This function is not implemented");
+            throw null;
+        }
+
+        public virtual IDataReader getAllData()
+        {
+            Debug.Log(Tag + "This function is not implemented");
+            throw null;
+        }
+
+        public virtual void deleteAllData()
+        {
+            Debug.Log(Tag + "This function is not implemnted");
+            throw null;
+        }
+
+        public virtual IDataReader getNumOfRows()
+        {
+            Debug.Log(Tag + "This function is not implemnted");
+            throw null;
+        }
+
+        //helper functions
+        public IDbCommand getDbCommand()
+        {
+            return dbConnection.CreateCommand();
+        }
+
+        public IDataReader getAllData(string table_name)
+        {
+            IDbCommand dbcmd = dbConnection.CreateCommand();
+            dbcmd.CommandText =
+                "SELECT * FROM " + table_name;
+            IDataReader reader = dbcmd.ExecuteReader();
+            return reader;
+        }
+
+        public void deleteAllData(string table_name)
+        {
+            IDbCommand dbcmd = dbConnection.CreateCommand();
+            dbcmd.CommandText = "DROP TABLE IF EXISTS " + table_name;
+            dbcmd.ExecuteNonQuery();
+        }
+
+        public IDataReader getNumOfRows(string table_name)
+        {
+            IDbCommand dbcmd = dbConnection.CreateCommand();
+            dbcmd.CommandText =
+                "SELECT COALESCE(MAX(id)+1, 0) FROM " + table_name;
+            IDataReader reader = dbcmd.ExecuteReader();
+            return reader;
+        }
+
+        public void close()
+        {
+            dbConnection.Close();
+        }
+    }
+}
